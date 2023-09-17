fb19e7530dede92ceb6c1a2b1ba55d9ac4503778
Athul Thomas <athuldev9727@gmail.com>
2023-09-17 19:26:14 +0530

    word cloud library improvements

--- app/src/main/java/com/athul97/wordcloud/MainActivity.kt
+++ app/src/main/java/com/athul97/wordcloud/MainActivity.kt
@@ -6,19 +6,18 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var wordCloudView: WordCloudView
     private val testData = listOf<WordCloud>(
-        WordCloud("dog",1,"#000"),
+        WordCloud("dog",1),
         WordCloud("cat",2),
-        WordCloud("parrot",3,"#000"),
+        WordCloud("parrot",3),
         WordCloud("crow",4),
-        WordCloud("cow",5,"#000")
+        WordCloud("cow",5)
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        wordCloudView = findViewById(R.id.wordcloud)
+        val wordCloudView: WordCloudView = findViewById(R.id.wordcloud)
         wordCloudView.setDataSet(testData)
         wordCloudView.getWordClicked {
             Log.e("CURRENT_WORD::",it.toString())
